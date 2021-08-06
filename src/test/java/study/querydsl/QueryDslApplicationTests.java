e660809247b0291035a7b35f633c0d5c8ef1835b
edin <edin@gccompany.co.kr>
2021-08-06 18:07:22 +0900

    테스트1

--- src/test/java/study/querydsl/QueryDslApplicationTests.java
+++ src/test/java/study/querydsl/QueryDslApplicationTests.java
@@ -1,4 +1,4 @@
-package study.querydsl;
+/package study.querydsl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +8,7 @@ class QueryDslApplicationTests {
 
     @Test
     void contextLoads() {
+        // 소스트리 푸시 계정 테스트1
     }
 
 }
